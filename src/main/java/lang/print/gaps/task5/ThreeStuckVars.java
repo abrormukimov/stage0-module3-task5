a0dcced9c46f702f062a00917e2e4a401588e533
abrormukimov <abrormm34@gmail.com>
2022-09-24 20:36:27 +0500

    Fix broken code

--- src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
+++ src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task5;
 
 public class ThreeStuckVars {
     public static void main(String[] args) {
+        int first = 1;
+        int second = 10;
+        int third = 100;
+        String result = String.format("%s%s%s", first, second, third);
+        System.out.println(result);
     }
 }
